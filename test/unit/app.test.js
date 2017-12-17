ca222775b07b6e94926f6003cb542d70de61c786
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-12-18 00:30:14 +1100

    Download repository into a specified directory

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -1,17 +1,31 @@
-const { expect } = require('chai')
+const td = require('testdouble')
 const App = require('../../lib/app')
 
 suite('App', () => {
-  test('it lets user specify a repository url', async () => {
+  test('it downloads the git repository into a specified directory', async () => {
+    const childProcess = td.object(['spawn'])
+    await createApp({ childProcess }).fetchRepository()
+
+    td.verify(
+      childProcess.spawn('git', ['clone', 'REPOSITORY_URL'], {
+        cwd: 'SAVE_DIR'
+      })
+    )
+  })
+
+  function createApp ({ childProcess } = {}) {
+    const extensionConfig = {
+      get: configName => configName === 'repositorySaveLocation' && 'SAVE_DIR'
+    }
     const vscode = {
       window: {
         showInputBox: () => Promise.resolve('REPOSITORY_URL')
+      },
+      workspace: {
+        getConfiguration: extensionName =>
+          extensionName === 'codeReading' && extensionConfig
       }
     }
-
-    const app = new App({ vscode })
-    const repositoryUrl = await app.fetchRepository()
-
-    expect(repositoryUrl).to.eql('REPOSITORY_URL')
-  })
+    return new App({ vscode, childProcess })
+  }
 })
