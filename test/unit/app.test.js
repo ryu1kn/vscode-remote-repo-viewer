301c58cbdb9525820b0cf62fac9686dd94ca87ea
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-12-17 17:29:53 +1100

    Let user to specify repository url

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -1,7 +1,17 @@
 const { expect } = require('chai')
+const App = require('../../lib/app')
 
 suite('App', () => {
-  test('sample', () => {
-    expect(1).to.eql(1)
+  test('it lets user specify a repository url', async () => {
+    const vscode = {
+      window: {
+        showInputBox: () => Promise.resolve('REPOSITORY_URL')
+      }
+    }
+
+    const app = new App({ vscode })
+    const repositoryUrl = await app.fetchRepository()
+
+    expect(repositoryUrl).to.eql('REPOSITORY_URL')
   })
 })
