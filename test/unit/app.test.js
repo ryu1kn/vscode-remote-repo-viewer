4fe80ed6a91facc770a6867a69921e38081d9956
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-12-19 06:54:27 +1100

    Propagate error

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -1,15 +1,33 @@
+const { expect } = require('chai')
 const td = require('testdouble')
 const App = require('../../lib/app')
 
 suite('App', () => {
   test('it downloads the git repository into a specified directory', async () => {
     const shellCommandRunner = td.object(['run'])
-    await createApp({ shellCommandRunner }).fetchRepository()
+    await createApp({
+      shellCommandRunner
+    }).fetchRepository()
 
     td.verify(
       shellCommandRunner.run('git', ['clone', 'REPOSITORY_URL'], {
         cwd: 'SAVE_DIR'
       })
+    )
+  })
+
+  test('it throws an exception if downloading encounters problem', () => {
+    const shellCommandRunner = {
+      run: () => Promise.reject(new Error('UNKNOWN'))
+    }
+    const app = createApp({
+      shellCommandRunner
+    })
+    return app.fetchRepository().then(
+      () => new Error('Should have been rejected'),
+      e => {
+        expect(e.message).to.eql('UNKNOWN')
+      }
     )
   })
 
@@ -26,6 +44,9 @@ suite('App', () => {
           extensionName === 'codeReading' && extensionConfig
       }
     }
-    return new App({ vscode, shellCommandRunner })
+    return new App({
+      vscode,
+      shellCommandRunner
+    })
   }
 })
