1c536ae87d19da7cee4792b5875ac0b494d0219a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-12-27 13:11:55 +1100

    Wrote integration test for ShellCommandRunner

---
+++ test/integration/shell-command-runner.test.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai')
+
+const childProcess = require('child_process')
+const ShellCommandRunner = require('../../lib/shell-command-runner')
+
+suite('ShellCommandRunner', () => {
+  test('it runs a given command and collects stdout @it', async () => {
+    const shellCommandRunner = createShellCommandRunner()
+    const output = await shellCommandRunner.run('echo', ['hello'])
+    expect(output).to.eql('hello\n')
+  })
+
+  test('it raises an exception if command failed @it', () => {
+    const shellCommandRunner = createShellCommandRunner()
+    return shellCommandRunner.run('ls', ['NON_EXISTING_FILE']).then(
+      () => new Error('Should have been failed'),
+      e => {
+        expect(e.message).to.eql(
+          'ls: NON_EXISTING_FILE: No such file or directory'
+        )
+      }
+    )
+  })
+
+  function createShellCommandRunner () {
+    return new ShellCommandRunner({ childProcess })
+  }
+})
