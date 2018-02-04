30d13794c7c625ddd74e25287306fc1d0c08da5f
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-02-04 12:52:16 +1100

    Do nothing if user doesn't select a repository

--- test/unit/commands/open-downloaded-repository.test.js
+++ test/unit/commands/open-downloaded-repository.test.js
@@ -3,15 +3,9 @@ const OpenDownloadedRepositoryCommand = require('../../../lib/commands/open-down
 
 suite('OpenDownloadedRepository', () => {
   test('it lets user to open a repository already downloaded', async () => {
-    const configStore = { repositorySaveDirectoryPath: 'SAVE_DIR/BAZ' }
-    const directoryLister = {
-      list: dirPath => dirPath === 'SAVE_DIR/BAZ' && ['REPO_A', 'REPO_B']
-    }
     const vscWindow = { showQuickPick: ([repo1]) => repo1 }
-    const repositoryDisplayer = { display: td.function() }
-    const command = new OpenDownloadedRepositoryCommand({
-      configStore,
-      directoryLister,
+    const repositoryDisplayer = td.object(['display'])
+    const command = createOpenDownloadedRepositoryCommand({
       repositoryDisplayer,
       vscWindow
     })
@@ -20,4 +14,36 @@ suite('OpenDownloadedRepository', () => {
 
     td.verify(repositoryDisplayer.display('SAVE_DIR/BAZ/REPO_A'))
   })
+
+  test('it does nothing if user does not select any repository', async () => {
+    const repositoryDisplayer = td.object(['display'])
+    const vscWindow = { showQuickPick: () => {} }
+    const command = createOpenDownloadedRepositoryCommand({
+      repositoryDisplayer,
+      vscWindow
+    })
+
+    await command.execute()
+
+    td.verify(repositoryDisplayer.display(), {
+      times: 0,
+      ignoreExtraArgs: true
+    })
+  })
+
+  function createOpenDownloadedRepositoryCommand ({
+    repositoryDisplayer,
+    vscWindow
+  }) {
+    const configStore = { repositorySaveDirectoryPath: 'SAVE_DIR/BAZ' }
+    const directoryLister = {
+      list: dirPath => dirPath === 'SAVE_DIR/BAZ' && ['REPO_A', 'REPO_B']
+    }
+    return new OpenDownloadedRepositoryCommand({
+      configStore,
+      directoryLister,
+      repositoryDisplayer,
+      vscWindow
+    })
+  }
 })
